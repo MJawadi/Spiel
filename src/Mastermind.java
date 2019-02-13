dd7b73b55ae63fe2a2223232bcfe081d0964da4f
Mohammad Jawadi <jawadi@corp.atacama.de>
2019-02-13 14:07:23 +0100

    12.02.2019

--- src/Mastermind.java
+++ src/Mastermind.java
@@ -1,78 +1,85 @@
 import java.util.*;
+import java.util.concurrent.ThreadLocalRandom;
 public class Mastermind {
 	
 	public static Scanner scan = new Scanner(System.in);
 
 	public static void main(String[] args) {
 		
-		ArrayList<Integer> randomList = new ArrayList<Integer>(4);
-		ArrayList<Integer> inputNumbers = new ArrayList<Integer>(4);
-		boolean result = true;
+		boolean geuse = false;
+		int userNumber=0;
+		int randomNumber=0;
 		
-		
-		for(int x=0; x<10; x++) {
-			if(x==0) {
-				getRandom(randomList);
-			}
-			inputNumbers.clear();
-			getNumber(inputNumbers);
-			
-			if(result == checking(randomList, inputNumbers, result)) {
-				System.out.print("Wollen Sie nochmal spielen?: ");
-				if(scan.nextLine().equals("y") || scan.nextLine().equals("Y")) {
-					x=0;
-				}else {
-					if(scan.nextLine().equals("n") || scan.nextLine().equals("N")) {
-						break;
-					}
-				}
+		randomNumber = getRandom(randomNumber);
+		while(true) {
+			userNumber = getNumber(userNumber);
+			if(checking(randomNumber, userNumber, geuse)) {
+				System.out.println("\nherzlich glückwunsch Sie haben Gewonnen");
+				break;
 			}
 		}
+		
+		
+		
+		
 	}
 	
 	
-	public static ArrayList<Integer> getNumber(ArrayList<Integer> inNum) {
+	public static int getNumber(int inNum) {
 		
+		int row = 0;
 		
-		for(int i=1; i<5; i++) {
-			System.out.print("\nZahl "+i+": (0/9): ");
-			int row = Mastermind.scan.nextInt();
-			if(row<10) {
-				if(i>1) {
-					if(inNum.contains(row)) {
-						System.out.println("Error!! Sie haben diese Nummer schon genohmen!!");
-						i--;
-					}else {
-						inNum.add(row);
-					}
+		while(true){
+			System.out.print("\ngeben Sie ein ziffer XXXX zahl: " );
+			row = Mastermind.scan.nextInt();
+			
+			if(row >= 1000 && row <= 9999) {
+				
+				int first=   row/1000;
+				int second=  row%1000/100;
+				int third=   row%100/10;
+				int fourth=  row%10;
+				
+				if(first==second || first ==third || first==fourth || second==third || second==fourth || third==fourth) {
+					
+					System.out.println("\nError!!! Sie haben geleiche ziffer gegeben!!");
+					row=0;
 				}else {
-					inNum.add(row);
+					inNum = row;
+					break;
 				}
+				
 			}else {
-				System.out.println("Error!!! Die Anzahl ist zu groß!!! noch mal versuchen.");
-				i--;
+				System.out.println("\nError!!! Die Anzahl ist groß oder klein !!! noch mal versuchen.");
 			}
-			System.out.println("\nIhre Zahlen: "+inNum);
 		}
 		return inNum;
+		
 	}
 	
 	
 	
 	
 	
-	public static ArrayList<Integer> getRandom(ArrayList<Integer> randList) {
+	public static int getRandom(int randNum) {
 		
-		Random rand = new Random();
-		for(int i=0; i<4; i++) {
-			int row = rand.nextInt(10);
-			if(randList.contains(row)) {
-				i--;
+		int row;
+		while(true) {
+			
+			row = ThreadLocalRandom.current().nextInt(1000, 9999);
+			int first=   row/1000;
+			int second=  row%1000/100;
+			int third=   row%100/10;
+			int fourth=  row%10;
+			
+			if(first==second || first ==third || first==fourth || second==third || second==fourth || third==fourth) {
+				row=0;
 			}else {
-				randList.add(row);
+				randNum = row;
+				break;
 			}
 		}
-		return randList;
+		return randNum;
 	}
 	
 	
@@ -80,29 +87,44 @@ public class Mastermind {
 	
 	
 	
-	public static boolean checking(ArrayList<Integer> randNum, ArrayList<Integer> userNum, boolean set) {
+	public static boolean checking(int randNum, int userNum, boolean set) {
 		
-		boolean guese = false;
-		set = guese;
-		int rightNumbers = 0;
-		for(int s=0; s<randNum.size(); s++) {
-			System.out.println("\n "+(s+1)+" Zahl: ");
-			if(randNum.get(s).equals(userNum.get(s))) {
-				System.out.print("(Direkter Treffer)");
-			}else {
-				if(randNum.contains(userNum.get(s))) {
-					System.out.print("(Indirekt Treffer)");
-				}
-			}
-			
-			if(randNum.get(s).equals(userNum.get(s))) {
-				rightNumbers++;
-				if(rightNumbers==4) {
-					System.out.println("\n\n\nGlückwunsch! Sie haben gewonnen!!!");
+		
+		int[] random = new int[4];
+		int[] user = new int[4];
+		char[] words = new char[4];
+		int count = 0;
+		
+		random[0] =   randNum/1000;
+		random[1] =   randNum%1000/100;
+		random[2] =   randNum%100/10;
+		random[3] =   randNum%10;
+		
+		user[0]  =   userNum/1000;
+		user[1]  =   userNum%1000/100;
+		user[2]  =   userNum%100/10;
+		user[3]  =   userNum%10;
+		
+		System.out.print("\nihre Erginesrn sind: ");
+		for(int i=0; i<4; i++) {
+			if(random[i]==user[i]) {
+				words[i]='R';
+				count++;
+				if(count==4) {
 					set = true;
 				}
+			}else {
+				words[i]='F';
 			}
-			System.out.print("\n");
+			
+			for(int b=0; b<4; b++) {
+				if(i!=b) {
+					if(user[i]==random[b]) {
+						words[i]='P';
+					}
+				}
+			}
+			System.out.print(words[i]);
 		}
 		return set;
 	}
--- src/Othello.java
+++ src/Othello.java
@@ -5,51 +5,28 @@ public class Othello {
 	
 	public static void main(String[] args) {
 		
+		int horizontal=8, vertical=8;
 		
-				
-		int[][] table = new int[8][8];
-		boolean check = true;
-		int player1=1, player2=2;
+		char[][] playerInput = new char[horizontal][vertical];
+		char[][] table = new char[horizontal][vertical];
+		boolean check = false;
+		char player1='X', player2='O';
 		
 		
 		
 		//table[4][7]=2;
-		table[3][3]=1;
-		//table[4][4]=1;
-		table[3][4]=1;
-		table[2][4]=2;
-		table[4][2]=1;
-		table[3][2]=2;
+		table[3][3]='X';
+		table[4][4]='X';
+		table[3][4]='O';
+		//table[2][4]='X';
+		table[4][3]='O';
+		//table[3][2]='+';
 		//table[5][4]=2;
 		//table[3][2]=2;
 		
-		while(true) {
-			
-			int[][] playerInput2 = new int[8][8];
-			int[][] playerInput1 = new int[8][8];
-			
-			checkPosiblePlaces(table, player2);
-			print(table);
-			System.out.println("Go X-->");
-			getInput(playerInput2, player2);
-			if(checkForEmpty(check, table, playerInput2)) {
-				rules(table, playerInput2, player2);
-			}else {
-				System.out.println("error");
-			}
-			
-			checkPosiblePlaces(table, player1);
-			print(table);
-			System.out.println("Go O-->");
-			getInput(playerInput1, player1);
-			if(checkForEmpty(check, table, playerInput1)) {
-				rules(table, playerInput1, player1);
-			}else {
-				System.out.println("error");
-			}
-			
-			
-		}
+		
+		;
+		print(checkPosiblePlaces(table, player1));
 
 		
 	}
@@ -98,51 +75,314 @@ public class Othello {
 		
 	}
 	
+
 	
-	
-	private static int[][] rules(int[][] cells, int[][] playerIn, int player){
-		
-		int otherPlayer = 1;
-		if(player==1) {
-			otherPlayer=2;
+	private static char[][] upDir(char[][] cells, int x, int y, char player) {
+
+		char otherPlayer = 'O';
+		if(player=='O') {
+			otherPlayer='X';
 		}
 		
-		for(int x=0; x<cells.length; x++) {
-			for(int y=0; y<cells.length; y++) {
-				
-				if(playerIn[x][y]==player) {
-					
-					
+		char[][] temp = new char[cells.length][cells.length];
+		int up = x;
+		int foundUp=0;
+		boolean getSecond = false;
+		while(up!=0) {
+			
+			if(getSecond==false && cells[up][y]==player) {
+				foundUp=1;
+				temp[up][y]=player;
+			}
+			if(foundUp==1) {
+				if(cells[up][y]==otherPlayer) {
+					getSecond = true;
+					temp[up][y]=player;
 				}
 			}
+			if(getSecond) {
+				if(cells[up][y]==player) {
+					foundUp=3;
+					temp[up][y]=player;
+					copyToAnother(cells, temp);
+				}
+			}
+			
+			if(up!=0) {
+				up--;
+			}
+		}
+		return cells;
+	}
+	
+	private static char[][] downDir(char[][] cells, int x, int y, char player) {
+
+		char otherPlayer = 'O';
+		if(player=='O') {
+			otherPlayer='X';
 		}
 		
+		char[][] temp = new char[cells.length][cells.length];
+		int down = x;
+		int foundUp=0;
+		boolean getSecond = false;
+		while(down!=cells.length) {
+			
+			if(getSecond==false && cells[down][y]==player) {
+				foundUp=1;
+				temp[down][y]=player;
+			}
+			if(foundUp==1) {
+				if(cells[down][y]==otherPlayer) {
+					getSecond = true;
+					temp[down][y]=player;
+				}
+			}
+			if(getSecond) {
+				if(cells[down][y]==player) {
+					foundUp=3;
+					temp[down][y]=player;
+					copyToAnother(cells, temp);
+				}
+			}
+			if(down!=cells.length) {
+				down++;
+			}
+		}
+		return cells;	
+	}
+	
+	private static char[][] leftDir(char[][] cells, int x, int y, char player) {
+
+		char otherPlayer = 'O';
+		if(player=='O') {
+			otherPlayer='X';
+		}
+		
+		char[][] temp = new char[cells.length][cells.length];
+		int left = y;
+		int foundUp=0;
+		boolean getSecond = false;
+		while(left!=0) {
+			
+			if(getSecond==false && cells[x][left]==player) {
+				foundUp=1;
+				temp[x][left]=player;
+			}
+			if(foundUp==1) {
+				if(cells[x][left]==otherPlayer) {
+					getSecond = true;
+					temp[x][left]=player;
+				}
+			}
+			if(getSecond) {
+				if(cells[x][left]==player) {
+					foundUp=3;
+					temp[x][left]=player;
+					copyToAnother(cells, temp);
+				}
+			}
+			if(left!=0) {
+				left--;
+			}
+		}
 		return cells;
 		
 	}
-
-	
-	
-	private static int[][] allDir(int[][] cells, int x, int y, int player, boolean found) {
-
 		
-		int otherPlayer = 1;
-		if(player==1) {
-			otherPlayer=2;
+	private static char[][] rightDir(char[][] cells, int x, int y, char player) {
+
+		char otherPlayer = 'O';
+		if(player=='O') {
+			otherPlayer='X';
 		}
-		int up = x;
-		int down = x;
+		
+		char[][] temp = new char[cells.length][cells.length];
 		int right = y;
-		int left = y;
-		boolean go = true;
-		int count = 0;
-		while(up!=0 || down!=cells.length || right!=cells.length || left!=0) {
+		int foundUp=0;
+		boolean getSecond = false;
+		while(right!=cells.length) {
 			
+			if(getSecond==false && cells[x][right]==player) {
+				foundUp=1;
+				temp[x][right]=player;
+			}
+			if(foundUp==1) {
+				if(cells[x][right]==otherPlayer) {
+					getSecond = true;
+					temp[x][right]=player;
+				}
+			}
+			if(getSecond) {
+				if(cells[x][right]==player) {
+					foundUp=3;
+					temp[x][right]=player;
+					copyToAnother(cells, temp);
+				}
+			}
+			if(right!=cells.length) {
+				right++;
+			}
+		}
+		return cells;
+		
+	}
+	
+	private static char[][] upLeftDir(char[][] cells, int x, int y, char player) {
+
+		char otherPlayer = 'O';
+		if(player=='O') {
+			otherPlayer='X';
+		}
+		
+		char[][] temp = new char[cells.length][cells.length];
+		int up = x;
+		int left = y;
+		int foundUp=0;
+		boolean getSecond = false;
+		
+		while(up!=0 || left!=0) {
+			
+			if(getSecond==false && cells[up][left]==player) {
+				foundUp=1;
+				temp[up][left]=player;
+			}
+			if(foundUp==1) {
+				if(cells[up][left]==otherPlayer) {
+					getSecond = true;
+					temp[up][left]=player;
+				}
+			}
+			if(getSecond) {
+				if(cells[up][left]==player) {
+					foundUp=3;
+					temp[up][left]=player;
+					copyToAnother(cells, temp);
+				}
+			}
 			if(up!=0) {
 				up--;
 			}
 			if(left!=0) {
 				left--;
+			}
+		}
+		return cells;
+		
+	}
+	
+	private static char[][] upRightDir(char[][] cells, int x, int y, char player) {
+
+		char otherPlayer = 'O';
+		if(player=='O') {
+			otherPlayer='X';
+		}
+		
+		char[][] temp = new char[cells.length][cells.length];
+		int up = x;
+		int right = y;
+		int foundUp=0;
+		boolean getSecond = false;
+		while(up!=0 || right!=cells.length) {
+			
+			if(getSecond==false && cells[up][right]==player) {
+				foundUp=1;
+				temp[up][right]=player;
+			}
+			if(foundUp==1) {
+				if(cells[up][right]==otherPlayer) {
+					getSecond = true;
+					temp[up][right]=player;
+				}
+			}
+			if(getSecond) {
+				if(cells[up][right]==player) {
+					foundUp=3;
+					temp[up][right]=player;
+					copyToAnother(cells, temp);
+				}
+			}
+			if(up!=0) {
+				up--;
+			}
+			if(right!=cells.length) {
+				right++;
+			}
+		}
+		return cells;
+	}
+	
+	private static char[][] downLeftDir(char[][] cells, int x, int y, char player) {
+
+		char otherPlayer = 'O';
+		if(player=='O') {
+			otherPlayer='X';
+		}
+		
+		char[][] temp = new char[cells.length][cells.length];
+		int down = x;
+		int left = y;
+		int foundUp=0;
+		boolean getSecond = false;
+		while(down!=cells.length || left!=0) {
+			
+			if(getSecond==false && cells[down][left]==player) {
+				foundUp=1;
+				temp[down][left]=player;
+			}
+			if(foundUp==1) {
+				if(cells[down][left]==otherPlayer) {
+					getSecond = true;
+					temp[down][left]=player;
+				}
+			}
+			if(getSecond) {
+				if(cells[down][left]==player) {
+					foundUp=3;
+					temp[down][left]=player;
+					copyToAnother(cells, temp);
+				}
+			}
+			if(left!=0) {
+				left--;
+			}
+			if(down!=cells.length) {
+				down++;
+			}
+		}
+		return cells;
+		
+	}
+	
+	private static char[][] downRightDir(char[][] cells, int x, int y, char player) {
+
+		char otherPlayer = 'O';
+		if(player=='O') {
+			otherPlayer='X';
+		}
+		char[][] temp = new char[cells.length][cells.length];
+		int down = x;
+		int right = y;
+		int foundUp=0;
+		boolean getSecond = false;
+		while(down!=cells.length || right!=cells.length) {
+			
+			if(getSecond==false && cells[down][right]==player) {
+				foundUp=1;
+				temp[down][right]=player;
+			}
+			if(foundUp==1) {
+				if(cells[down][right]==otherPlayer) {
+					getSecond = true;
+					temp[down][right]=player;
+				}
+			}
+			if(getSecond) {
+				if(cells[down][right]==player) {
+					foundUp=3;
+					temp[down][right]=player;
+					copyToAnother(cells, temp);
+				}
 			}
 			if(right!=cells.length) {
 				right++;
@@ -150,171 +390,129 @@ public class Othello {
 			if(down!=cells.length) {
 				down++;
 			}
-			
-			
-			if(cells[up][y]==player) {
-				found = true;
-				break;
-			}else {
-				if(cells[down][y]==player){
-					
-				}
-			}
-			if(cells[x][right]==otherPlayer) {
-				
-			}else {
-				if(cells[x][left]==otherPlayer) {
-					
-				}
-			}
-			if(cells[up][left]==otherPlayer) {
-				
-			}else {
-				if(cells[up][right]==otherPlayer) {
-					
-				}
-			}
-			if(cells[down][left]==otherPlayer) {
-				
-			}else {
-				if(cells[down][right]==otherPlayer) {
-					
-				}
-			}
-			
 		}
 		return cells;
-		
 	}
-	
-	//private static int[][] turnCells(int[][] cells, )
 
 
-	public static boolean checkForEmpty(boolean result, int[][] cells, int[][] player){
-		
-		
+	public static boolean checkForEmpty(boolean result, char[][] cells, char[][] player){
 		
 		for(int x=0; x<cells.length; x++) {
 			for(int y=0; y<cells.length; y++) {
-				if((player[x][y]==1 || player[x][y]==2) && (cells[x][y]==1 || cells[x][y]==2)) {
+				if((player[x][y]=='X' || player[x][y]=='O') && (cells[x][y]=='X' || cells[x][y]=='O')) {
 					result = false;
 					System.out.println("\n\tThis place is not empty!!! plase take another one!!");
 					break;
 				}
-				if((player[x][y]==1 || player[x][y]==2) && cells[x][y]!=3) {
+				if((player[x][y]=='X' || player[x][y]=='O') && cells[x][y]!='+') {
 					System.out.println("Wrong Place!!!");
 					result = false;
 					break;
 				}
 			}
 		}
-		
 		return result;
 	}
 	
 	
-	
-	public static int[][] checkPosiblePlaces(int[][] cells, int player) {
+	public static char[][] checkPosiblePlaces(char[][] cells, char player) {
 		
-		int player2=1;
-		
-		if(player==1) {
-			player2=2;
+		char otherPlayer = 'O';
+		if(player=='O') {
+			otherPlayer='X';
 		}
 		
-		for(int r=0; r<cells.length; r++) {
-			for(int u=0; u<cells.length; u++) {
-				if(cells[r][u]==3) {
-					cells[r][u]=0;
+		for(int l=0; l<cells.length; l++) {
+			for(int w=0; w<cells.length; w++) {
+				if(cells[l][w]=='+') {
+					cells[l][w]=' ';
 				}
 			}
 		}
 		
-		
-		
 		for(int x=0; x<cells.length; x++) {
 			for(int y=0; y<cells.length; y++) {
-				if((x>0 && x<7) && (y>0 && y<7)) {
+				if((x>0 && x<cells.length-1) && (y>0 && y<cells.length-1)) {
 					
-					if(cells[x][y]==player2) {
+					if(cells[x][y]==otherPlayer) {
 						for(int a=x-1; a<x+2; a++) {
 							for(int b=y-1; b<y+2; b++) {
-								if(cells[a][b]==0) {
-									cells[a][b]=3;
+								if(cells[a][b]==' ') {
+									cells[a][b]='+';
 								}
 							}
 						}
 					}
 				}else {
-					if(cells[x][y]==player2) {
+					if(cells[x][y]==otherPlayer) {
 						if(x==0 && y==0) {
 							for(int s=x; s<x+2; s++) {
 								for(int j=y; j<y+2; j++) {
-									if(cells[s][j]==0) {
-										cells[s][j]=3;
+									if(cells[s][j]==' ') {
+										cells[s][j]='+';
 									}
 								}
 							}
 						}
-						if(x==0 && y==7) {
+						if(x==0 && y==cells.length) {
 							for(int s=x; s<x+2; s++) {
 								for(int j=y-1; j<y+1; j++) {
-									if(cells[s][j]==0) {
-										cells[s][j]=3;
+									if(cells[s][j]==' ') {
+										cells[s][j]='+';
 									}
 								}
 							}
 						}
-						if(x==7 && y==0) {
+						if(x==cells.length && y==0) {
 							for(int s=x-1; s<x+1; s++) {
 								for(int j=y; j<y+2; j++) {
-									if(cells[s][j]==0) {
-										cells[s][j]=3;
+									if(cells[s][j]==' ') {
+										cells[s][j]='+';
 									}
 								}
 							}
 						}
-						if(x==7 && y==7) {
+						if(x==cells.length && y==cells.length) {
 							for(int s=x-1; s<x+1; s++) {
 								for(int j=y-1; j<y+1; j++) {
-									if(cells[s][j]==0) {
-										cells[s][j]=3;
+									if(cells[s][j]==' ') {
+										cells[s][j]='+';
 									}
 								}
 							}
 						}
-						if(x==0 && (y>=1 && y<=6)) {
+						if(x==0 && (y>0 && y<cells.length)) {
 							for(int u=x; u<x+2; u++) {
 								for(int b=y-1; b<y+2; b++) {
-									if(cells[u][b]==0) {
-										cells[u][b]=3;
+									if(cells[u][b]==' ') {
+										cells[u][b]='+';
 									}
 								}
 							}
 						}
-						if(x==7 && (y>=1 && y<=6)) {
+						if(x==cells.length && (y>0 && y<cells.length)) {
 							for(int u=x-1; u<x+1; u++) {
 								for(int b=y-1; b<y+2; b++) {
-									if(cells[u][b]==0) {
-										cells[u][b]=3;
+									if(cells[u][b]==' ') {
+										cells[u][b]='+';
 									}
 								}
 							}
 						}
-						if((x>=1 && x<=6) && y==0) {
+						if((x>0 && x<cells.length) && y==0) {
 							for(int u=x-1; u<x+2; u++) {
 								for(int b=y; b<y+2; b++) {
-									if(cells[u][b]==0) {
-										cells[u][b]=3;
+									if(cells[u][b]==' ') {
+										cells[u][b]='+';
 									}
 								}
 							}
 						}
-						if((x>=1 && x<=6) && y==7) {
+						if((x>0 && x<cells.length) && y==cells.length) {
 							for(int u=x-1; u<x+2; u++) {
 								for(int b=y-1; b<y+1; b++) {
-									if(cells[u][b]==0) {
-										cells[u][b]=3;
+									if(cells[u][b]==' ') {
+										cells[u][b]='+';
 									}
 								}
 							}
@@ -323,48 +521,41 @@ public class Othello {
 				}
 			}
 		}
-		
-		
 		return cells;
 	}
 	
 	
+	public static char[][] copyToAnother(char[][] cells, char[][] temporary){
+		
+		for(int x=0; x<cells.length; x++) {
+			for(int y=0; y<cells.length; y++) {
+				cells[x][y]=temporary[x][y];
+			}
+		}
+		return cells;
+	}
 	
-	public static void print(int[][] cells){
+	
+	private static void print(char[][] cells){
 		
-		
-		
-		int player1=1, player2=2;
 		char word = 'A';
 		System.out.println("\n\t\tOthello Game Boeard by M.Jawadi");
 		System.out.println("\t_____________________________________________________");
 		System.out.println("\t| * |  1  |  2  |  3  |  4  |  5  |  6  |  7  |  8  |");
 		System.out.println("\t|———————————————————————————————————————————————————|");
+		
 		for(int x=0; x<8; x++) {
 			System.out.print("\t| "+word +" |  ");
 			word +=1;
 			for(int y=0; y<8; y++) {
-				if(cells[x][y]==player1) {
-					System.out.print("O");
-				}else {
-					if(cells[x][y]==player2) {
-						System.out.print("X");
-					}else {
-						if(cells[x][y]==3) {
-							System.out.print("+");
-						}else {
-							System.out.print(".");
-						}
-					}
-				}
+				
+				System.out.print(cells[x][y]);
 				
 				System.out.print("  |  ");
 			}
 			System.out.print("\n\t|———————————————————————————————————————————————————|");
 			System.out.print("\n");
 		}
-		
-		
 	}
 
 }
